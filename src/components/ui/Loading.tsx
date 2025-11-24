866d1ce4ee05553a259800250e301a6a04575bc1
teboc <bobllor@yahoo.com>
2025-11-23 20:06:13 -0600

    added loading component

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -2,8 +2,8 @@ import { JSX } from "react";
 
 export default function Loading(): JSX.Element{
     return (
-        <>
-        
-        </>
+        <div className="flex w-full justify-center items-center">
+            <div className="w-10 h-10 border-4 border-blue-300 rounded-2xl loader shadow-2xl" />
+        </div>
     )
 }
