86c01326a0ebd4a408cc6a02bfbf5096ee8893f2
teboc <bobllor@yahoo.com>
2025-11-24 00:42:37 -0600

    added csv support, improved error handling and ui changes due to errors, added helper functions for handling files

--- src/components/FileComponents/utils/index.ts
+++ src/components/FileComponents/utils/index.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 import { toastError, toastSuccess } from '../../../toastUtils.ts';
 import '../../../pywebview.ts';
-import { UploadedFilesProps, FileStatus, GenerateCSVProps } from './types.ts';
+import { UploadedFilesProps, FileStatus, GenerateCSVProps, FileType } from './types.ts';
 import { Response } from '../../../pywebviewTypes.ts';
 import { generateId } from "../../../utils.ts"; 
 
@@ -11,20 +11,30 @@ export function onFileChange(
     setUploadedFiles: React.Dispatch<React.SetStateAction<Array<UploadedFilesProps>>>){
         const id: string = Date.now().toString();
         const file: File|undefined = event.currentTarget.files?.[0];
-
-        if(!file) return;
-
-        const fileName: string = file.name;
-
-        if(fileName.split(".").at(-1)?.toLowerCase() != 'xlsx'){
-            toastError("Only Excel files (.xlsx) are accepted.");
-            return;
-        }
-
+        
         // some weird thing with input elements. if i recall this was an issue in my last project too.
         event.currentTarget.value = "";
 
-        setUploadedFiles(prev => [...prev, {id: id, name: fileName, file: file, status: "none"}]);
+        if(!file) return;
+
+        const res: Record<string, string> = validateFile(file);
+
+        if(res.status == "error"){
+            toastError(res.message);
+
+            return;
+        }
+
+        setUploadedFiles(prev => [
+            ...prev, 
+            {
+                id: id, 
+                name: file.name, 
+                file: file, 
+                status: "none", 
+                fileType: res.fileType as FileType,
+            }
+        ]);
 }
 
 //** Reads a file and generates a Base64 string for decoding. */
@@ -50,9 +60,21 @@ export async function uploadFile(
     flattenCsv: boolean = false): Promise<boolean>{
     event.preventDefault();
 
+    // helper function for handling errors on a file
+    const handleFileError = (msg: string, fileId: string) => {
+        toastError(msg);
+
+        setFileArr(prev => prev.map(fileObj => {
+            if(fileObj.id == fileId){
+                return {...fileObj, status: "error"};
+            }
+
+            return fileObj;
+        }))
+    }
+
     // by default it will assume true, it is modified inside the csv generation call.
     let boolOut: boolean = true;
-
     if(fileArr.length == 0){
         toastError("No files were submitted.");
         return true;
@@ -62,16 +84,23 @@ export async function uploadFile(
 
     // setting up the b64 strings to send to the backend.
     for(const file of fileArr){
-        const fileExtension: string|undefined = file.file?.name.split('.').at(-1);
+        const res: Record<string, string> = validateFile(file.file);
 
-        if(!file || !fileExtension || fileExtension?.toLowerCase() != 'xlsx'){
-            toastError(`Only Excel files (.xlsx) are supported, got file type .${fileExtension}.`);
+        if(res.status == "error"){
+            handleFileError(res.message, file.id);
+            
             continue;
         }
-        
-        const b64: string|ArrayBuffer|null = await getBase64(file.file);
 
-        csvResponseArr.push({fileName: file.name, b64: b64 as string, id: file.id})
+        try{
+            const b64: string|ArrayBuffer|null = await getBase64(file.file);
+            
+            csvResponseArr.push({fileName: file.name, b64: b64 as string, id: file.id})
+        }catch (error){
+            console.log(error);
+
+            handleFileError(`An error occurred while reading ${file.name}`, file.id);
+        } 
     }
 
     // used with flattenCsv flag, it will be regenerated if false.
@@ -123,7 +152,23 @@ export function onDragDrop(event: DragEvent,
 
     if(!file) return;
 
-    setUploadedFiles(prev => [...prev, {id: id, name: file.name, file: file, status: "none"}]);
+    const res: Record<string, string> = validateFile(file);
+
+    if(res.status == "error"){
+        toastError(res.message);
+        return;
+    }
+
+    setUploadedFiles(prev => [
+        ...prev, 
+        {
+            id: id, 
+            name: file.name, 
+            file: file, 
+            status: "none",
+            fileType: res.fileType as FileType
+        }
+    ]);
 }
 
 //** Removes an object from the uploaded files state on a matching ID. */
@@ -134,4 +179,35 @@ export function deleteFileEntry(
     setUploadedFiles(prev => prev.filter(ele => {
         return ele.id != fileID;
     }));
+}
+
+/**
+ * Validates a File object for file uploads
+ * @param file A File object
+ */
+function validateFile(file: File): Record<string, string>{
+    const res: Record<string, string> = {
+        "status": "success", 
+        "message": "Valid file",
+        "fileType": "",
+    };
+
+    const fileName: string = file.name;
+    const fileExt: string = fileName.split(".").at(-1)?.toLowerCase() || "invalid";
+
+    const validExtenions: Set<string> = new Set([
+        "xlsx", 
+        "csv"
+    ]);
+
+    if(!validExtenions.has(fileExt)){
+        res.status = "error";
+        res.message = "Only files ending in .csv and .xlsx are accepted.";
+
+        return res;
+    }
+
+    res.fileType = fileExt;
+
+    return res;
 }
