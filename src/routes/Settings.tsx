2dbc613d64d7f6b23524c301e19dc34608abe2fd
teboc <bobllor@yahoo.com>
2025-12-02 02:10:18 -0600

    added Password route for settings

--- src/routes/Settings.tsx
+++ src/routes/Settings.tsx
@@ -3,13 +3,14 @@ import BackgroundBlur from "../components/ui/BackgroundBlur";
 import Navigation from "../components/SettingsComponents/Navigation";
 import Options from "../components/SettingsComponents/Options";
 import { SettingsButton } from "../components/SettingsComponents/types";
-import { FaBuilding, FaClipboardList, FaHome, FaList } from "react-icons/fa";
+import { FaBuilding, FaClipboardList, FaHome, FaKey, FaList } from "react-icons/fa";
 
 const iconSize: number = 20;
 const buttons: Array<SettingsButton> = [
     {label: "General", url: "", icon: <FaHome size={iconSize}/>},
     {label: "Headers", url: "headers-mapping", icon: <FaList size={iconSize} />},
     {label: "Organization", url: "opco-mapping", icon: <FaBuilding size={iconSize} />},
+    {label: "Password", url: "password", icon: <FaKey size={iconSize} />},
     {label: "Text Template", url: "template", icon: <FaClipboardList size={iconSize} />},
 ]
 
@@ -20,7 +21,7 @@ export default function Settings({setShowSetting}:
     return (
         <>
             <div
-            className={`absolute w-200 h-150 z-4 bg-white settings-modal-border`}> 
+            className={`absolute w-200 h-150 z-4 bg-white settings-modal-border default-shadow`}> 
                 <Navigation buttons={buttons} setShowSetting={setShowSetting}
                 selectedSetting={selectedSetting} setSelectedSetting={setSelectedSetting} />
                 <Options />
