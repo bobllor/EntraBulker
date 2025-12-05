f3fe709165cb6338638d43f445e8b6c39696ab54
teboc <bobllor@yahoo.com>
2025-12-04 20:51:00 -0600

    added two column name support for parsing the df

--- backend/core/parser.py
+++ backend/core/parser.py
@@ -1,5 +1,6 @@
 from typing import Any, Callable
 from support.types import Response, HeaderMap
+from copy import deepcopy
 import pandas as pd
 import support.utils as util
 
@@ -17,7 +18,7 @@ class Parser:
         # lower all column names.
         self.df.rename(mapper=lambda x: x.lower(), axis=1, inplace=True)
 
-    def validate(self, default_headers: HeaderMap) -> Response:
+    def validate(self, default_headers: HeaderMap, *, two_name_column_support: bool = False) -> Response:
         '''Validate the DataFrame and its headers. It will return a Response indicating an
         error/success and a message with the error if applicable.
         
@@ -27,11 +28,31 @@ class Parser:
                 Dictionary that maps internal variable names to user-defined names. The keys
                 are the internal names, the values are user-defined names. Used to validate
                 column headers.
+            
+            two_name_column_support: bool, default `False`
+                A boolean used to handle the column for the client name being split
+                into *two columns* (`First name`/`Last name`) instead of a single `Full name` column.
+                By default it is `False`. If true, then it will create a new column `Full name` and remove
+                the two columns for normalization.
         '''
+        default_headers_copy: HeaderMap = deepcopy(default_headers)
+        base_name_column: str = default_headers_copy["name"]
+
+        # must remove otherwise column check will fail
+        if not two_name_column_support:
+            del default_headers_copy["first_name"]
+            del default_headers_copy["last_name"]
+        else:
+            # this will be added back in the check_df_columns step
+            # after combining the first_name and last_name columns.
+            del default_headers_copy["name"]
+
+        # check_df_columns must be the last number in the dict
+        # any other functions can be in any order
         func_dict: dict[int, dict[str, Any]] = {
-            0: {"func": self._check_duplicate_headers, "args": default_headers},
+            0: {"func": self._check_duplicate_headers, "args": default_headers_copy},
             1: {"func": self._check_duplicate_columns, "args": None},
-            2: {"func": self._check_df_columns, "args": default_headers},
+            2: {"func": self._check_df_columns, "args": default_headers_copy},
         }
 
         res: Response = util.generate_response(message="")
@@ -47,6 +68,16 @@ class Parser:
 
             if res["status"] == "error":
                 return res
+            
+            if i == len(func_dict) - 1 and two_name_column_support:
+                first_name_col: str = default_headers["first_name"]
+                last_name_col: str = default_headers["last_name"]
+
+                parsed_name_column: pd.Series = self._create_full_name_series(first_name_col, last_name_col)
+
+                # NOTE: no columns are dropped since every other column but `name` and `opco`
+                # are ignored.
+                self.df[base_name_column] = parsed_name_column
 
         res["message"] = "Successful validation"
 
@@ -67,7 +98,7 @@ class Parser:
         bad_rows: list[int] = []
 
         for i, data in enumerate(self.get_rows(col_name)):
-            if not isinstance(data, str):
+            if not isinstance(data, str) or data.strip() == "":
                 bad_rows.append(i)
         
         self.df.drop(index=bad_rows, axis=0, inplace=True)
@@ -112,6 +143,31 @@ class Parser:
     
     def get_df(self) -> pd.DataFrame:
         return self.df
+    
+    def _create_full_name_series(self, first_name_column: str, last_name_column: str) -> pd.Series:
+        '''Creates a Series containing data of the full name of the client. This is used to combine
+        two name columns into one for normalization in parsing.
+
+        Prior to this function call, the DataFrame must be validated first or this will raise a KeyError exception.
+
+        Parameters
+        ----------
+            first_name_column: str
+                The column key representing the `First name` column.
+            
+            last_name_column: str
+                The column key representing the `Last name` column.
+        '''
+        first_names: pd.Series = self.df[first_name_column].apply(func=lambda x: x.strip())
+        last_names: pd.Series = self.df[last_name_column].apply(func=lambda x: x.strip())
+
+        # if drop_empty_rows is called, this ensures the rows will get dropped.
+        first_names = first_names.fillna("")
+        last_names = last_names.fillna("")
+
+        full_names: pd.Series = first_names + " " + last_names
+
+        return full_names
     
     def _check_duplicate_headers(self, headers: HeaderMap) -> Response:
         '''Checks the given HeaderMap for duplicate values. The HeaderMap will be reversed to
