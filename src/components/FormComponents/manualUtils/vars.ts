33c56bed4f4c2cc466feb5e089ce70b3d5458a33
teboc <bobllor@yahoo.com>
2025-12-06 01:51:14 -0600

    moved tableHeaders into ManualTable

--- src/components/FormComponents/manualUtils/vars.ts
+++ src/components/FormComponents/manualUtils/vars.ts
@@ -5,5 +5,3 @@ export const formInputs: Array<{name: string, label: string}> = [
     {name: 'name-input', label: nameLabel},
     {name: 'opco-input', label: opcoLabel},
 ]
-
-export const tableHeaders: Array<string> = ['Name', 'Operating Company'];
