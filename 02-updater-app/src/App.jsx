73ee4b365370a94f96bb665553ce2d055ff84bcb
teboc <bobllor@yahoo.com>
2025-12-16 00:53:19 -0600

    started framework for progress bar

--- 02-updater-app/src/App.jsx
+++ 02-updater-app/src/App.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css'
+import ProgressBar from './components/ProgressBar'
+
+const functions = [];
+const funcLength = functions.length;
 
 export default function App() {
+  const [innerProgressBarWidth, setInnerProgressBarWidth] = useState(1);
+
+  useEffect(() => {
+    // all functions returns a Response
+
+  }, [])
 
   return (
     <>
+      <div className="w-screen h-screen flex flex-col justify-end items-center">
+        <ProgressBar innerProgressBarWidth={innerProgressBarWidth} /> 
+        <button onClick={() => setInnerProgressBarWidth(prev => prev+20)}>Test button</button>
+      </div>
     </>
   )
 }
