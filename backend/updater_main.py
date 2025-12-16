20b04ba19a4ce9e6bb8a1c3e66dea2ece022ef5a
teboc <bobllor@yahoo.com>
2025-12-16 00:51:22 -0600

    updated window code, added set_window for destruction

--- backend/updater_main.py
+++ backend/updater_main.py
@@ -1,14 +1,18 @@
 from logger import Log
 from api.updater_api import UpdaterAPI
+from core.updater import Updater
 from support.vars import PROJECT_ROOT
 import webview
 
 if __name__ == "__main__":
-    api: UpdaterAPI = UpdaterAPI(project_root=PROJECT_ROOT)
-    size: tuple[int, int] = (300, 200)
+    updater: Updater = Updater(PROJECT_ROOT) 
+    api: UpdaterAPI = UpdaterAPI(updater)
+    size: tuple[int, int] = (400, 250)
 
     title: str = f'EntraBulker Updater'
     url: str = 'http://localhost:5172/' 
 
-    webview.create_window(title, url, js_api=api, min_size=size, resizable=False)
+    window: webview.Window = webview.create_window(title, url, js_api=api, width=size[0], height=size[1], min_size=size, resizable=False)
+    api.set_window(window)
+
     webview.start(debug=True)
