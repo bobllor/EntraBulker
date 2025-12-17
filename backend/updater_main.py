8082f73390ec99610a8bde47fb73fe212e301ea6
teboc <bobllor@yahoo.com>
2025-12-17 12:47:13 -0600

    added args framework for future use

--- backend/updater_main.py
+++ backend/updater_main.py
@@ -2,12 +2,15 @@ from logger import Log
 from api.updater_api import UpdaterAPI
 from core.updater import Updater
 from support.vars import PROJECT_ROOT
-import webview
+import webview, sys
 
 if __name__ == "__main__":
     updater: Updater = Updater(PROJECT_ROOT) 
     api: UpdaterAPI = UpdaterAPI(updater)
     size: tuple[int, int] = (400, 250)
+    
+    # TODO: add a flag checker to ensure this cant be run normally, only through the main app.
+    flag: str = sys.argv[-1]
 
     title: str = f'EntraBulker Updater'
     url: str = 'http://localhost:5172/' 
