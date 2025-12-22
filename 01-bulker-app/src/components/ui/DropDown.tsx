540e957e061ab4d4e2783e066e277bb749556804
bobllor <bobllor@yahoo.com>
2025-12-22 02:21:59 -0600

    updated DropDown UI

--- 01-bulker-app/src/components/ui/DropDown.tsx
+++ 01-bulker-app/src/components/ui/DropDown.tsx
@@ -5,7 +5,7 @@ export default function DropDown({obj, objId, defaultValue, func}: DropDownProps
     return (
         <>
             <select
-            className="outline-none min-w-[30%] max-w-[30%]"
+            className="outline-1 min-w-[30%] max-w-[30%] rounded-sm p-1"
             tabIndex={-1}
             defaultValue={defaultValue}
             onChange={(e) => handleOnChangeSelect(e, objId, func)}>
