58bae7070ba00dc3ce25abd1a7d3498b6a59923f
bobllor <bobllor@yahoo.com>
2025-12-22 02:14:37 -0600

    added new code to handle prod/dev environments, added code for local web server

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,35 @@
 from core.json_reader import Reader
+from core.server import LocalServer
 from api.api import API
 from logger import Log
 from support.vars import DEFAULT_HEADER_MAP, DEFAULT_OPCO_MAP, DEFAULT_SETTINGS_MAP, PROJECT_ROOT, VERSION
+from support.utils import init_window, is_prod
 import webview
 
 EXCEL_FILE: str = 'excel-mapping.json'
 SETTINGS_FILE: str = 'settings.json'
 OPCO_FILE: str = "opco-mapping.json"
-EXCEL_PATH: str = f'config/{EXCEL_FILE}'
-SETTINGS_PATH: str = f'config/{SETTINGS_FILE}'
-OPCO_PATH: str = f"config/{OPCO_FILE}"
-LOGS_PATH: str = f"logs"
+
+EXCEL_PATH: str = f'{str(PROJECT_ROOT)}/config/{EXCEL_FILE}'
+SETTINGS_PATH: str = f'{str(PROJECT_ROOT)}/config/{SETTINGS_FILE}'
+OPCO_PATH: str = f"{str(PROJECT_ROOT)}/config/{OPCO_FILE}"
+LOGS_PATH: str = f"{str(PROJECT_ROOT)}/logs"
 
 if __name__ == '__main__':
-    logger: Log = Log()
+    # will use the npm port, this is changed if in prod
+    port: int = 5173
+    url: str = f"http://localhost:{port}"
+
+    if is_prod():
+        server: LocalServer = LocalServer("madist")
+        server.run()
+        url = server.url
+
+    debug, log_path = init_window()
+
+    logger: Log = Log(log_dir=log_path)
+
+    logger.debug(f"Log path: {log_path} | URL: {url} | Debug: {debug} | Root: {PROJECT_ROOT}")
 
     excel_reader: Reader = Reader(EXCEL_PATH, defaults=DEFAULT_HEADER_MAP, update_only=True, logger=logger, project_root=PROJECT_ROOT)
     settings_reader: Reader = Reader(SETTINGS_PATH, defaults=DEFAULT_SETTINGS_MAP, update_only=True, logger=logger, project_root=PROJECT_ROOT)
@@ -29,8 +45,7 @@ if __name__ == '__main__':
     size: tuple[int, int] = (1080, 720)
 
     title: str = f'EntraBulker {VERSION}'
-    url: str = 'http://localhost:5173/'
 
     window: webview.Window = webview.create_window(title, url, js_api=api, min_size=size)
     api.set_window(window)
-    webview.start(debug=True)
+    webview.start(debug=debug)
