e0c2fb29dac71f99f3094ace722a81c53268ffa8
bobllor <bobllor@yahoo.com>
2026-05-31 17:25:56 -0500

    added comments for variables, new types and fields for POST user data

--- backend/core/graph_types.py
+++ backend/core/graph_types.py
@@ -1,17 +1,45 @@
-from typing import TypedDict
+from typing import TypedDict, Literal
+from dataclasses import dataclass
 
 class PasswordProfileJson(TypedDict):
     forceChangePasswordNextSignIn: bool
     password: str
 
+type UserType = Literal["Member", "Guest"]
+
 class CreateUserJson(TypedDict):
-    accountEnabled: str
+    # The status of the account. Can be true or false.
+    accountEnabled: bool
+
+    # The name in displayed in the directory. This is the full name of the user.
     displayName: str
-    # i think this is the user of user@domain.com?
+
+    # The mail alias of the user. This should be `user` of `user@domain.com`, as it is by default.
     mailNickname: str
-    passwordProfile: PasswordProfileJson
-    # e.g. user@domain.com
+
+    # The "email" of the user, or the principal name. For example, `user@domain.com`.
     userPrincipalName: str
+
+    # The type of user, this is either Member or Guest. This will be a setting to enable
+    # in the options. It will apply to every user being created.
+    userType: UserType
+
+    # The password profile of the user.
+    passwordProfile: PasswordProfileJson
+
+    # The first name of the user.
+    givenName: str
+
+    # The last name of the user.
+    surname: str
+
+@dataclass
+class RequestErrorResponse:
+    '''A class representing the error response of a Graph request.'''
+    code: str = ""
+    message: str = ""
+    date: str = ""
+    request_id: str = ""
 
 JsonHeaders = TypedDict("JsonHeaders",
     {
