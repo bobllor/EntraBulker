633e8cc9173caf83b48664e1e41a1bdd474ba729
bobllor <bobllor@yahoo.com>
2026-06-01 18:11:46 -0500

    added decorator for handling request errors, updated code and logging

--- backend/core/graph.py
+++ backend/core/graph.py
@@ -1,14 +1,42 @@
 from msal import PublicClientApplication
+from msal.oauth2cli.oauth2 import BrowserInteractionTimeoutError
 from logger import Log
-from typing import Any
+from typing import Any, Callable
 from support.types import Response
+from core.types.graph import CreateUserJson, JsonHeaders, RequestErrorResponse
 import requests
 import support.utils as utils
-from core.graph_types import CreateUserJson, JsonHeaders, RequestErrorResponse
 
 GRAPH_BASE_URL: str = "https://graph.microsoft.com/v1.0"
 GRAPH_CREATE_USER_URL: str = f"{GRAPH_BASE_URL}/users"
 GRAPH_ME_URL: str = f"{GRAPH_BASE_URL}/me"
+
+REQ_TIMEOUT: int = 20
+
+def requests_handler(f: Callable[[Any], Response]) -> Response:
+    '''Decorator used to wrap requests methods in a try-except and logs with
+    errors if one occurs.
+
+    The method must be a class with a logging instance self.log, and the method must return a Response.
+
+    Any error exceptions will return the error `status`, the error `message`, and a None `content`.
+    '''
+    def wrapper(self, *args, **kwargs):
+        try:
+            res: Response = f(self, *args, **kwargs)
+
+            return res
+        except requests.exceptions.Timeout:
+            self.log.exception(f"Request timeout")
+            return utils.generate_response("error", message="Request timed out", content=None)
+        except requests.exceptions.ConnectionError:
+            self.log.exception(f"Connection failure")
+            return utils.generate_response("error", message="Connection error while performing request", content=None)
+        except Exception:
+            self.log.exception(f"An unexpected error occurred during a request")
+            return utils.generate_response("error", message="An unknown error occurred", content=None)
+    
+    return wrapper
 
 class Graph:
     '''Class used for Microsoft Graph based operations.'''
@@ -24,28 +52,43 @@ class Graph:
         # set in authenticate on succcessful token retrieval
         self.bearer: str = ""
 
+        # set inside authenticate
+        self.app: PublicClientApplication = None
+
         # least privilege scope that allows writing to entra, do not change!
         self._scopes: list[str] = ["User.ReadWrite.All"]
-    
+
+    @requests_handler 
     def is_authenticated(self) -> Response:
         '''Checks if the client is authenticated. It will return a Response with the
         status of the authentication in content.
+
+        If the token is None, then it will return not authenticated. A request is sent
+        to check if a token exists.
+
+        The Response status will always be success, unless an exception occurs during
+        the request itself in which case it will be an error.
         '''
+        not_res: Response = utils.generate_response("success", message="Not authenticated", content=False)
+        if self.token is None:
+            return not_res
+
         res: Response = utils.generate_response("success", message="Authenticated", content=True)
 
         headers: JsonHeaders = {
             "authorization": self.bearer,
         }
 
-        getres: requests.Response = requests.get(GRAPH_ME_URL, headers=headers) 
+        # due to it being a GET the timeout can afford to be less
+        timeout: str = REQ_TIMEOUT // 2
+        getres: requests.Response = requests.get(GRAPH_ME_URL, headers=headers, timeout=timeout) 
         json: dict[str, Any] = getres.json()
 
         if not getres.ok:
             err: RequestErrorResponse = self.get_error(json)
+            self.log.info(f"Authentication status: {err} | Code: {err.code} | Message: {err.message}")
 
-            self.log.warning(f"Failed to check authentication status: {err} | Code: {err.code} | Message: {err.message}")
-
-            return utils.generate_response("error", message="Not authenticated", content=False)
+            return not_res
 
         return res
 
@@ -59,17 +102,24 @@ class Graph:
             authority=self._auth_url,
         )
 
+        self.app = app
+
         if self.token is None:
-            timeout_seconds: int = 300
-            res: dict[str, Any] = app.acquire_token_interactive(self._scopes, timeout=timeout_seconds)
-            token_key: str = "access_token"
+            timeout_seconds: int = 180
+            try:
+                auth_res: dict[str, Any] = app.acquire_token_interactive(self._scopes, timeout=timeout_seconds)
+                token_key: str = "access_token"
+            except BrowserInteractionTimeoutError:
+                self.log.info(f"Authentication timeout reached: User did not complete the flow in time")
+
+                return utils.generate_response("error", message="Authentication timed out")
             
             if token_key in res:
-                self.token = res.get(token_key)
+                self.token = auth_res.get(token_key)
                 self.bearer = f"Bearer {self.token}"
             else:
-                errorStr: str = f"error={res.get('error')};desc={res.get('error_description')};id={res.get('correlation_id')}"
-                self.log.warning(
+                errorStr: str = f"error={auth_res.get('error')};desc={auth_res.get('error_description')};id={auth_res.get('correlation_id')}"
+                self.log.error(
                     f"Failed to authenticate Graph API | {errorStr}"
                 )
                 res = utils.generate_response("error", message="Failed to authenticate")
@@ -83,12 +133,16 @@ class Graph:
             res["message"] = "Already authenticated"
 
         return res
-    
+
+    @requests_handler 
     def create_users(self, users: list[CreateUserJson]) -> Response:
         '''Sends a POST request and creates the users. Errors that occur will not interrupt other users
         given in the list but will be logged.
 
-        Any error that will occur automatically will mark the Response as an error.
+        Any error that will occur automatically will mark the Response as an error. 
+        
+        If all users  given failed to POST for whatever reason, then it will return an *error*. 
+        If there are a handful of failed POST requests, then it will return a *warning*. 
         '''
         end_res: Response = utils.generate_response(message=f"Created users")
         headers: JsonHeaders = {
@@ -105,7 +159,7 @@ class Graph:
             "failed users": {"users": failed_users},
         }
         for user_json in users:
-            post_res: requests.Response = requests.post(GRAPH_CREATE_USER_URL, json=user_json, headers=headers)
+            post_res: requests.Response = requests.post(GRAPH_CREATE_USER_URL, json=user_json, headers=headers, timeout=REQ_TIMEOUT)
             data: dict[str, Any] = post_res.json()
 
             self.log.debug(f"POST response: {data}")
@@ -119,9 +173,12 @@ class Graph:
                 created_users.append(user_json['userPrincipalName'])
 
         if len(failed_users) > 0:
-            end_res["status"] = "error"
+            end_res["status"] = "warning"
             end_res["message"] = f"Failed to add {len(failed_users)}/{len(users)} user(s) with Graph API"
-        
+        if len(failed_users) == len(users):
+            end_res["status"] = "error"
+            end_res["message"] = f"Failed to add all given user(s) with Graph API"
+
         post_user_info["created users"]["Count"] = len(created_users)
         post_user_info["failed users"]["Count"] = len(failed_users)
         
@@ -130,11 +187,14 @@ class Graph:
         return end_res
     
     def get_error(self, d: dict[str, Any]) -> RequestErrorResponse:
-        '''Parses a dictionary and creates a new RequestErrorResponse.'''
-        code: str = utils.get_key(d, "code") or ""
-        err_msg: str = utils.get_key(d, "message") or ""
-        date: str = utils.get_key(d, "date") or ""
-        request_id: str = utils.get_key(d, "request-id") or ""
+        '''Parses a dictionary and creates a new RequestErrorResponse.
+        
+        Missing values will be an None if missing.
+        '''
+        code: str = utils.get_key(d, "code")
+        err_msg: str = utils.get_key(d, "message")
+        date: str = utils.get_key(d, "date")
+        request_id: str = utils.get_key(d, "request-id")
 
         err = RequestErrorResponse(code, err_msg, date, request_id)
         
