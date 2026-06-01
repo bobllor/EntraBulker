40f795556453ff5ef5a2e3e577b5fabe9081e93b
bobllor <bobllor@yahoo.com>
2026-06-01 01:17:35 -0500

    updated GraphMap inverted user domain to member only domain type, updated varialbe comments

--- backend/support/types.py
+++ backend/support/types.py
@@ -81,20 +81,17 @@ class GraphMap(TypedDict):
     # if this option is true.
     enable_graph: bool
 
-    # Creates the users as `Member` or as `Guest`. By default, all users are
-    # created as Member in Entra ID.
+    # Creates the users as `Member` or as `Guest`. This will cause all users
+    # to be created of one type. By default, all users are created as Member in 
+    # Entra ID.
     user_type: UserType
 
-    # A CSV-style string used to list domains to ignore the user type set
-    # in user_type. The users found with the domain will be created with the opposite
-    # user type. In other words, `Member` -> `Guest` and `Guest` -> `Member`.
+    # A CSV-style string containing domains that will always be of the type `Member`.
+    # This only applies if the `user_type` is set to `Guest`.
     #
-    # For example, if the user type is `Guest` and the CSV value is
-    # `@example.com,another.example.com`, then domains ending in these two values
-    # will not be converted to `Guest`` and remain as `Member`.
-    # Vice versa, if the user type is `Member` then they will instead be converted
-    # into `Guest`.
-    domain_ignore_user_type_csv: str
+    # For example, if the CSV value is `@example.com,another.example.com`, then the
+    # then all domains that do not match these two domains will be converted to `Guest`.
+    member_type_domain_csv: str
 
 class HeaderMap(TypedDict):
     opco: str
