25a7c9f8ed1da0cba20851b013160449afc01d93
bobllor <bobllor@yahoo.com>
2026-06-01 01:16:59 -0500

    updated GraphMap defaults

--- backend/support/vars.py
+++ backend/support/vars.py
@@ -84,5 +84,5 @@ DEFAULT_GRAPH_MAP: GraphMap = {
     "tenant_id": "",
     "enable_graph": False,
     "user_type": "Guest",
-    "domain_ignore_user_type_csv": "",
+    "member_type_domain_csv": "",
 }
