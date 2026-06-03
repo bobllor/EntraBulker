c93d0148d0d0f4ea9eb1b57c2918e18d49658752
bobllor <bobllor@yahoo.com>
2026-06-03 15:30:05 -0500

    updated DataText code, new optValueElement and justification props

--- 01-bulker-app/src/components/ui/DataText.tsx
+++ 01-bulker-app/src/components/ui/DataText.tsx
@@ -5,7 +5,8 @@ import { toastSuccess } from "../../toastUtils";
  * Creates a span JSX element that displays the value given.
  * @returns A JSX element
  */
-export default function DataText({label = "", maxValueLength = 30, value, enableCopy = false}: DataTextProps): JSX.Element{
+export default function DataText({label = "", maxValueLength = 30, value, 
+    enableCopy = false, optValueElement, justification = "start"}: DataTextProps): JSX.Element{
     return (
         <span
         onClick={() => {
@@ -15,11 +16,12 @@ export default function DataText({label = "", maxValueLength = 30, value, enable
                 });
             }
         }}
-        className={`text-xs text-ellipsis overflow-hidden whitespace-nowrap w-68 h-4
-            flex items-center justify-start rounded-xl px-1
+        className={`text-xs text-ellipsis overflow-hidden whitespace-nowrap w-fit max-w-68 h-4
+            flex items-center justify-${justification} rounded-xl px-1
             ${enableCopy && "hover:bg-gray-500/25"}`}
         title={value.length > maxValueLength ? value : ""}>
             {label + value}
+            {optValueElement != undefined && optValueElement}
         </span>
     )
 }
@@ -34,11 +36,19 @@ type DataTextProps = {
      */
     value: string
     /**
+     * An optional element that displays after the value. This is often an icon or image.
+     */
+    optValueElement?: JSX.Element
+    /**
      * The maximum length of the value text before it cuts off with ellipsis.
      * This does not include the label concatenation.
      * This is optional and defaults to 20 characters.
      */
     maxValueLength?: number
+    /**
+     * The justification of the elements in the span. By default it is start.
+     */
+    justification?: "start" | "center"
     /**
      * Flag used to allow copying to the clipboard by clicking on the element. This
      * also will a hover effect when the mouse is over. By default it is false.
