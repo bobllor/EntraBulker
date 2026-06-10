e2bbdec1b8198e13d409566afb85ada8169bcc56
bobllor <bobllor@yahoo.com>
2026-06-10 12:21:33 -0500

    imported pywebview to prevent typescript errors

--- 01-bulker-app/src/components/FileComponents/store/useProcessingErrorStore.ts
+++ 01-bulker-app/src/components/FileComponents/store/useProcessingErrorStore.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { GraphError, Response } from "../../../pywebviewTypes";
+import "../../../pywebview";
 
 type ProcessingErrorStore = {
     errors: Array<ProcessingErrorObject>
