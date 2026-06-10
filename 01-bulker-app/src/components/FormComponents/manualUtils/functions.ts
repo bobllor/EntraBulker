fcd1cbd1f3cb0690c638266f37b7882fddead29b
bobllor <bobllor@yahoo.com>
2026-06-10 12:16:07 -0500

    removed unused function

--- 01-bulker-app/src/components/FormComponents/manualUtils/functions.ts
+++ 01-bulker-app/src/components/FormComponents/manualUtils/functions.ts
@@ -1,8 +1,10 @@
 import React from "react";
 import { InputDataProps } from "./types";
 import { ManualData } from "./types";
-import { toastError, toastSuccess } from "../../../toastUtils";
+import { toastError, toastSuccess, toastWarning } from "../../../toastUtils";
 import "../../../pywebview";
+import { Response } from "../../../pywebviewTypes";
+import { useProcessingErrorStore } from "../../FileComponents/store/useProcessingErrorStore";
 
 /**
  * Validates the form inputs to ensure no duplicates are in either field, if it fails then
@@ -19,21 +21,6 @@ export function validateInput(event: React.ChangeEvent<HTMLInputElement>,
         setInputData(prev => {
             return {...prev, [elementName]: currValue}
         })
-}
-
-export async function submitManualEntry(manualData: Array<ManualData>): Promise<void>{
-    if(manualData.length == 0){
-        toastError("No entries found");
-        return;
-    }
-
-    let res: {status: string, message: string} = await window.pywebview.api.generate_manual_csv(manualData);
-
-    if(res.status == 'success'){
-        toastSuccess(res.message);
-    }else{
-        toastError(res.message);
-    }
 }
 
 export function handleDivClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>, 
